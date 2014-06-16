c6d0a58947b57bd0a1ade4451a3bf51a96ec70fa
Satnam Ram <satnamram@googlemail.com>
2014-06-16 15:29:25 +0200

    improved file open/close dialog

--- dialog.c
+++ dialog.c
@@ -1,6 +1,9 @@
 #include "cedit.h"
 
-void dialog_savefile(){cmd_new("save file:", "savefile");}
-void dialog_savedfile(){draw_info("file saved");}
-void dialog_openfile(){cmd_new("open file:", "openfile");}
-void dialog_openedfile(){draw_info("file opened");}
+void dialog_savefile()            {cmd_new("save file:", "savefile");}
+void dialog_savefile_success()    {draw_info("file saved");}
+void dialog_savefile_failure()    {draw_info("error saving file");}
+
+void dialog_openfile()            {cmd_new("open file:", "openfile");}
+void dialog_openfile_success()    {draw_info("file opened");}
+void dialog_openfile_failure()    {draw_info("error opening file");}
