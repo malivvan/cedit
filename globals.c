4c4a3ea9499d253ddfc4bbf3298b98f0c16d6f9a
Vanessa Ram <malivvan@gmail.com>
2014-06-27 02:15:59 +0200

    LINEENDMARKER global defined - will mark user defined line, leaving this at zero will disable the function

--- globals.c
+++ globals.c
@@ -2,6 +2,7 @@ File *CF = 0;			// ptr to current file
 Cmd *CMD = 0;			// ptr to current command line
 size_t LINESIZE = 100;		// byte to be malloced
 size_t TABSIZE = 8;		// default tab lenght
+size_t LINEENDMARKER = 81;	// default end of line 0 == option disabled
 
 uint16_t FG = TB_WHITE;		// DEFAULT FG
 uint16_t BG = TB_BLACK;		// DEFAULT BG
