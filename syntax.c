ee5fb04260c279eed877f4e093ac4c6784d1d927
Satnam Ram <satnamram@googlemail.com>
2014-07-08 00:55:12 +0200

    removed redundant code in syntax.c

--- syntax.c
+++ syntax.c
@@ -3,8 +3,7 @@
 static size_t delay = 0;
 static size_t BC_lock = 0;
 static size_t ILC_lock = 0;
-static size_t QML_lock = 0;
-static size_t QMS_lock = 0;
+
 
 /*
  * decides if there is an inline comment or not
@@ -15,11 +14,6 @@ void syntax_ILC(Line *l, size_t bcnt, size_t len)
 	size_t x;
 	size_t isILC;
 	char *ftype;
-
-	/* do not do stuff if there is an ongoing blockcomment */
-	if(BC_lock == 1) return;
-	if(QML_lock == 1) return;
-	if(QMS_lock == 1) return;
 
 	/* reset @ every new line because this is an inline comment */
 	if(bcnt == 0) syntax_reset();
@@ -66,11 +60,6 @@ void syntax_BC(Line *l, size_t bcnt, size_t len)
 	size_t i;
 	size_t isBC;
 	char *ftype;
-
-	/* do not do stuff if there is an ongoing inline comment */
-	if(ILC_lock == 1) return;
-	if(QML_lock == 1) return;
-	if(QMS_lock == 1) return;
 
 	/* syntax highlighting depending on filetype */
 	char **bc = 0;
@@ -122,46 +111,39 @@ void syntax_BC(Line *l, size_t bcnt, size_t len)
 	}
 }
 
-/* quotation mark large (") */
-void syntax_QML(Line *l, size_t bcnt, size_t len)
+
+
+
+/*
+ * if quotation is detected count the utf8 chars until the next quotation mark
+ * of the same kind and set the calculated value as delay
+ */
+void syntax_QM(Line *l, size_t bcnt, size_t len)
 {
+	char c;
 	size_t i;
+	size_t d;
 
-	if(BC_lock == 1) return;
-	if(ILC_lock == 1) return;
+	if(l->c[bcnt] != 39 && l->c[bcnt] != 34) return;
 
-	if(l->c[bcnt] == 34){
-		if(QML_lock == 1){
-			delay = 1;
-		} else {
-			QML_lock = 1;
+	c = l->c[bcnt];
+	d = 1;
+
+	for(i = 1; bcnt+i <= l->blen; i += tb_utf8_char_length(l->c[bcnt+i])){
+		d++;
+		if(l->c[bcnt+i] == c){
 			FG = TB_BLUE | TB_BOLD;
+			delay = d;
 		}
 	}
 }
 
-/* quotation mark small (') */
-void syntax_QMS(Line *l, size_t bcnt, size_t len)
-{
-	size_t i;
-	if(l->c[bcnt] == 39){
-		for(i = 1; bcnt+i <= l->blen; i += tb_utf8_char_length(l->c[bcnt+i])){
-			if(l->c[bcnt+i] == 39){
-				FG = TB_BLUE | TB_BOLD;
-				delay = i;
-			}
-		}
-	}
-}
-
+/*
+ * hightlights every number that is not enclosed by invalid chars
+ */
 void syntax_NUM(Line *l, size_t bcnt, size_t len)
 {
 	size_t i;
-
-	if(BC_lock == 1) return;
-	if(ILC_lock == 1) return;
-	if(QML_lock == 1) return;
-	if(QMS_lock == 1) return;
 
 	if(isNumber(l->c[bcnt])){
 		/* check front */
@@ -192,29 +174,44 @@ void syntax_NUM(Line *l, size_t bcnt, size_t len)
 	}
 }
 
-/* for type like int, size_t etc.*/
-void syntax_TYPE(Line *l, size_t bcnt, size_t len)
+/*
+ * parses every word, decides if it is a type or a reserved word and
+ * hightlights it
+ */
+void syntax_WORD(Line *l, size_t bcnt, size_t len)
 {
 	size_t i;
 	size_t x;
 	size_t isType;
 	char *ftype;
 
-	/* syntax highlighting depending on filetype */
+	/* type will hold datatypes, res(erved) will hold reserved words and
+	   word will point to one of them */
+	char **word;
 	char **type = 0;
+	char **res = 0;
+
+	/* .c */
+	char *res_c[2]     =   {"break", 0};
 	char *type_c[11]   =   {"signed", "unsigned","char","short", "int"
 		                "long", "float", "double", "void", "size_t", 0};
-	char *type_go[3]  =    {"aa", "bbb", 0};
+
+	/* .go */
+	char *res_go[2]    =   {"break", 0};
+	char *type_go[3]   =   {"aa", "bbb", 0};
+
+	/* decide which words to highlight depending on filetype */
 	ftype = misc_filetype();
 	if(ftype == 0){
 		return;
 	} else {
-		if(strcmp(ftype, "c" ) == 0)        type = type_c;
-		if(strcmp(ftype, "go") == 0)        type = type_go;
+		if(strcmp(ftype, "c" ) == 0) { type = type_c; res = res_c;     }
+		if(strcmp(ftype, "go") == 0) { type = type_go; res = res_go;   }
 		free(ftype);
 	}
-	if(type == 0) return;
+	if(type == 0 || res == 0) return;
 
+	/* detect words and highlight them if they match */
 	for(x = 0; type[x] != 0; x++){
 		isType = 1;
 		if(bcnt + strlen(type[x]) <= l->blen){
@@ -245,8 +242,6 @@ void syntax_reset()
 {
 	BC_lock = 0;
 	ILC_lock = 0;
-	QML_lock = 0;
-	QMS_lock = 0;
 	FG = TB_WHITE | TB_BOLD;
 	BG = TB_BLACK;
 }
@@ -260,12 +255,13 @@ void syntax_all(Line *line, size_t bcnt, size_t len)
 	if(delay > 0) delay--;
 	if(delay > 0) return;
 
+	/* comments: block and inline comments preclude each other */
+	if(!BC_lock) syntax_ILC(line, bcnt, len);
+	if(!ILC_lock) syntax_BC(line, bcnt, len);
 
-
-	syntax_ILC(line, bcnt, len);
-	syntax_BC(line, bcnt, len);
+	/* numbers, quotations and words: only if there is no ongoing comment */
+	if(BC_lock || ILC_lock) return;
+	syntax_QM(line, bcnt, len);
 	syntax_NUM(line, bcnt, len);
-	syntax_QML(line, bcnt, len);
-	syntax_QMS(line, bcnt, len);
-	syntax_TYPE(line, bcnt, len);
+	syntax_WORD(line, bcnt, len);
 }
